7a79aca9c2c76806a0f65c365d1c78e099c0f55e
AdolfoMD <adolf.md.uam@gmail.com>
2021-06-09 22:38:17 -0500

    modificando proveedores.php

--- proveedores.php
+++ proveedores.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Personal</title>
+        <title>Proveedores</title>
         <link rel="stylesheet" href="styles.css" />
         <script
             src="https://kit.fontawesome.com/5a8aa1d43e.js"
@@ -24,7 +24,7 @@
                         <h2>Registrar nuevo proveedor</h2><br>
                         <form action="personal.php" method="POST">
                             
-                            <label for="nombre">Nombre completo:</label><br>
+                            <label for="nombre">Nombre proveedor:</label><br>
                             <input class="inputtxt" type="text" name="nombre" maxlength="50"><br><br><hr><br>
 
                             <h3>Direccion</h3><br>
@@ -46,26 +46,11 @@
                                     <label for="municipio">Municipio:</label><br>
                                     <input class="inputtxt" type="text" name="municipio" maxlength="50">
                                 </div>
-                            </div><hr>
-
-                            <div class="fila">
                                 <div class="col" style="width: 50%;">
-                                    <label for="cargo">Cargo:</label><br>
-                                    <input class="inputtxt" type="text" name="cargo" minlength="8" maxlength="9">
-                                </div>
-                                <div class="col" style="width: 50%;">
-                                    <label for="telefono">Telefono:</label><br>
-                                    <input class="inputtxt" type="text" name="telefono" minlength="10" maxlength="10">
-                                </div>
-                                <div class="col" style="width: 50%;">
-                                    <label for="correo">Correo:</label><br>
-                                    <input class="inputtxt" type="email" name="correo">
-                                </div>
-                                <div class="col" style="width: 50%;">
-                                    <label for="pass">Contraseña:</label><br>
-                                    <input class="inputtxt" type="password" name="pass">
-                                </div>
-                            </div><br>
+                                <label for="telefono">Telefono:</label><br>
+                                <input class="inputtxt" type="text" name="telefono" minlength="10" maxlength="10">
+                            </div>
+                            </div>
 
                             <input style="font-size:15px;width:20%;" type="submit" name="add" value="Añadir">
 
